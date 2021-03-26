3ab5462b4706e496c088fe87aa295741620baf43
nbaradar <naderb1992@gmail.com>
2021-03-26 00:46:31 -0400

    init

---
+++ HashTable.java
@@ -0,0 +1,20 @@
+import java.util.LinkedList;
+import java.util.ArrayList;
+
+public class HashTable {
+    //array of linked lists
+    //hash function
+
+    //to insert a key
+    //1. compute the hashcode
+    //2. map the hashcode to an index in the array (ex. hash(key)%array_length)
+    //3. At this index store the key and value. Linkedlist is used to avoid collision
+
+    LinkedList value[] = new LinkedList[1];
+
+
+    public int hash(String value){
+        return 0;
+    }
+}
+
